5dc3e883111d417881e3eebd9f09fe4c2f037a69
Daniel Danciu <dd@daedalean.ai>
2020-11-30 14:52:15 +0100

    Fix includes in tensor sketch. Test skeletons for tensor sketches

--- sketch/tensor.hpp
+++ sketch/tensor.hpp
@@ -2,6 +2,8 @@
 
 #include <cmath>
 #include <cstdio>
+#include <numeric>
+#include <random>
 
 #include "util/multivec.hpp"
 
@@ -9,15 +11,20 @@ namespace ts { // ts = Tensor Sketch
 
 
 /**
- * Computes tensor sketches for a given sequence.
- * @tparam T the type of elements in the sequences to be sketched.
+ * Computes tensor sketches for a given sequence as described in
+ * https://www.biorxiv.org/content/10.1101/2020.11.13.381814v1
+ * @tparam T the type of elements in the sketch and in the sequences to be sketched.
  */
 template <class T>
 class Tensor {
   public:
     /**
-     * @param set_size the number of elements in S,
-     * @param sketch_dim the number of components (elements) in the sketch vector.
+     * @param set_size the number of elements in S
+     * @param sketch_dim the number of components (elements) in the sketch vector
+     * @param num_phases number of phases to be used for modular arithmetic
+     * @param num_bins number of bins for discretization
+     * @param tup_len the length of the subsequences considered for sketching (denoted by t in the
+     * paper).
      */
     Tensor(T set_size, size_t sketch_dim, size_t num_phases, size_t num_bins, size_t tup_len)
         : sketch_dim(sketch_dim),
@@ -28,16 +35,16 @@ class Tensor {
         rand_init();
     }
 
-    Vec<T> compute(const std::vector<T> &seq) {
+    Vec<T> compute(const std::vector<T> &kmers) {
         Vec<T> sketch;
         Timer::start("tensor_sketch");
         sketch = Vec<T>(sketch_dim, 0);
         for (size_t m = 0; m < sketch_dim; m++) {
             auto cnt = new2D<double>(tup_len + 1, num_phases, 0);
             cnt[0][0] = 1; // base case
-            for (size_t i = 0; i < seq.size(); i++) {
+            for (size_t i = 0; i < kmers.size(); i++) {
                 for (int32_t t = (int32_t)tup_len - 1; t >= 0; t--) {
-                    auto pi = iphase[m][t][seq[i]];
+                    auto pi = iphase[m][t][kmers[i]];
                     for (size_t p = 0; p < num_phases; p++) {
                         auto shift = (p + pi) % num_phases;
                         cnt[t + 1][shift] += cnt[t][p];
@@ -49,9 +56,6 @@ class Tensor {
                                            (double)0);
             double norm = l1(top_cnt);
             prod = prod / norm;
-            //            int exp;
-            //            frexp(prod, &exp);
-            //            embedding[m]= exp * sgn(prod);
             T bin = std::upper_bound(bins.begin(), bins.begin() + num_bins, prod) - bins.begin();
             if (num_bins == 0) {
                 sketch[m] = prod;
@@ -70,7 +74,6 @@ class Tensor {
         std::mt19937 gen(rd());
         std::uniform_int_distribution<int> unif_iphase(0, num_phases - 1);
         std::uniform_real_distribution<double> unif(0, 1);
-        double pie = std::atan(1) * 2;
 
         iphase = new3D<int>(sketch_dim, tup_len, set_size);
         icdf = new2D<double>(sketch_dim, num_phases);
@@ -89,7 +92,7 @@ class Tensor {
         }
         bins = Vec<double>(num_bins);
         for (size_t b = 0; b < num_bins; b++) {
-            bins[b] = std::tan(pie * (((double)b + .5) / num_bins - .5));
+            bins[b] = std::tan(M_PI * (((double)b + .5) / num_bins - .5));
         }
         bins.push_back(std::numeric_limits<double>::max());
         bins.insert(bins.begin(), -std::numeric_limits<double>::min());
---
+++ tests/sketch/test_tensor_slide.cpp
@@ -0,0 +1,31 @@
+#include "sketch/tensor_slide.hpp"
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <random>
+
+namespace {
+
+using namespace ts;
+using namespace ::testing;
+
+constexpr uint32_t alphabet_size = 4;
+const uint32_t set_size = int_pow<uint32_t>(alphabet_size, 3); // k-mers of length 3
+constexpr uint32_t sketch_dim = 2;
+constexpr uint32_t tuple_length = 3;
+constexpr uint32_t num_phases = 2;
+constexpr uint32_t num_bins = 255;
+constexpr uint32_t window_length = 32;
+constexpr uint32_t stride = 8;
+constexpr uint32_t offset = 0;
+
+TEST(TensorSlide, Empty) {
+    TensorSlide<uint8_t> under_test(set_size, sketch_dim, num_phases, num_bins, tuple_length, window_length, stride, offset);
+    Vec2D<uint8_t> sketch;
+    under_test.compute(std::vector<uint8_t>(), sketch);
+    //ASSERT_THAT(sketch, ElementsAre(0, 0, 0));
+}
+
+} // namespace
+
