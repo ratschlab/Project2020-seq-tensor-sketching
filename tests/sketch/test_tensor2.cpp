09cfaf3c399e0cfb117327738b86d7cfd1b3d005
Daniel Danciu <dd@daedalean.ai>
2020-12-03 12:00:48 +0100

    More tests for tensor sketch: one char, tuple-1 chars

--- tests/sketch/test_tensor2.cpp
+++ tests/sketch/test_tensor2.cpp
@@ -76,7 +76,7 @@ TEST(Tensor2, FullStringRandomChars) {
 
 /**
  * If a sequence contains identical characters, its sketch will be +/-1 in one position and 0 in all
- * others.
+ * others, because all subsequences of length t are identical.
  */
 TEST(Tensor2, SameChars) {
     std::mt19937 gen(321567);
@@ -94,6 +94,51 @@ TEST(Tensor2, SameChars) {
             }
             ASSERT_EQ(1, std::abs(std::accumulate(sketch.begin(), sketch.end(), 0)))
                     << "Dim=" << sketch_dimension << " t=" << tuple_len;
+        }
+    }
+}
+
+/**
+ * If a sequence contains distinct characters, the the tensor sketch for t=1 will contain multiples
+ * of (1/alphabet_size), because T(a)=1/alphabet_size for all characters a.
+ */
+TEST(Tensor2, DistinctCharsTuple1) {
+    std::mt19937 gen(321567);
+    constexpr uint8_t tuple_len = 1;
+    std::vector<uint8_t> sequence(alphabet_size);
+    std::iota(sequence.begin(), sequence.end(), 0);
+    for (uint32_t sketch_dimension = 3; sketch_dimension < 10; ++sketch_dimension) {
+        Tensor2<uint8_t, double> under_test(alphabet_size, sketch_dimension, tuple_len);
+
+        Vec<double> sketch = under_test.compute(sequence);
+        ASSERT_EQ(sketch.size(), sketch_dimension);
+        for (uint32_t i = 0; i < sketch_dimension; ++i) {
+            double factor = sketch[i] / (1. / alphabet_size);
+            ASSERT_NEAR(0, std::round(factor) - factor, 1e-3);
+        }
+    }
+}
+
+/**
+ * If a sequence of length seq_len contains distinct characters, the the tensor sketch for
+ * t=seq_len-1 will contain multiples of (1/t), because T(a)=1/t for all the seq_len subsequences of
+ * length seq_len-1
+ */
+TEST(Tensor2, DistinctCharsTupleTMinus1) {
+    std::mt19937 gen(321567);
+    for (uint32_t tuple_len = 1; tuple_len < 10; ++tuple_len) {
+        const uint8_t alphabet_size = tuple_len + 1;
+        std::vector<uint8_t> sequence(alphabet_size);
+        std::iota(sequence.begin(), sequence.end(), 0);
+        for (uint32_t sketch_dimension = 3; sketch_dimension < 10; ++sketch_dimension) {
+            Tensor2<uint8_t, double> under_test(alphabet_size, sketch_dimension, tuple_len);
+
+            Vec<double> sketch = under_test.compute(sequence);
+            ASSERT_EQ(sketch.size(), sketch_dimension);
+            for (uint32_t i = 0; i < sketch_dimension; ++i) {
+                double factor = sketch[i] / (1. / alphabet_size);
+                ASSERT_NEAR(0, std::round(factor) - factor, 1e-3);
+            }
         }
     }
 }
