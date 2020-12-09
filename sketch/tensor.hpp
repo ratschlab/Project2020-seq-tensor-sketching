4a2bcb333576789c087080cf5951dc1541a2a7a8
Daniel Danciu <dd@daedalean.ai>
2020-12-09 09:44:21 +0100

    Further simplification and cleanup of tensor.hpp

--- sketch/tensor.hpp
+++ sketch/tensor.hpp
@@ -23,8 +23,22 @@ class Tensor {
      * in the paper
      */
     Tensor(seq_type alphabet_size, size_t sketch_size, size_t subsequence_len)
-        : alphabet_size(alphabet_size), sketch_size(sketch_size), subsequence_len(subsequence_len) {
-        rand_init();
+        : alphabet_size(alphabet_size),
+          sketch_size(sketch_size),
+          subsequence_len(subsequence_len),
+          hashes(new2D<seq_type>(subsequence_len, alphabet_size)),
+          signs(new2D<bool>(subsequence_len, alphabet_size)) {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<seq_type> rand_hash2(0, sketch_size - 1);
+        std::uniform_int_distribution<seq_type> rand_bool(0, 1);
+
+        for (size_t h = 0; h < subsequence_len; h++) {
+            for (size_t c = 0; c < alphabet_size; c++) {
+                hashes[h][c] = rand_hash2(gen);
+                signs[h][c] = rand_bool(gen);
+            }
+        }
     }
 
     /**
@@ -33,41 +47,46 @@ class Tensor {
      * @return an array of size #sketch_size containing the sequence's sketch
      */
     std::vector<double> compute(const std::vector<seq_type> &seq) {
-        // T1 is T(x,p,+1), T2 is T(x,p,-1) in the paper
-        auto T1 = new2D<double>(subsequence_len + 1, sketch_size, 0);
-        auto T2 = new2D<double>(subsequence_len + 1, sketch_size, 0);
+        // Tp corresponds to T+, Tm to T- in the paper; Tp[0], Tm[0] are sentinels and contain the
+        // initial condition for empty strings; Tp[p], Tm[p] represent the partial sketch when
+        // considering hashes h1...hp, over the prefix x1...xi. The final result is then
+        // Tp[t]-Tm[t], where t is #sequence_len
+        auto Tp = new2D<double>(subsequence_len + 1, sketch_size, 0);
+        auto Tm = new2D<double>(subsequence_len + 1, sketch_size, 0);
 
         // the initial condition states that the sketch for the empty string is (1,0,..)
-        T1[0][0] = 1;
+        Tp[0][0] = 1;
         for (uint32_t i = 0; i < seq.size(); i++) {
-            // must traverse in reverse order, to avoid overwriting the values of T1 and T2 before
+            // must traverse in reverse order, to avoid overwriting the values of Tp and Tm before
             // they are used in the recurrence
-            for (uint32_t t = std::min(i + 1, (uint32_t)subsequence_len); t >= 1; --t) {
-                double z = t / (i + 1.0); // probability that the last index is i
-                seq_type r = hashes[t - 1][seq[i]];
-                bool s = signs[t - 1][seq[i]];
+            for (uint32_t p = std::min(i + 1, (uint32_t)subsequence_len); p >= 1; --p) {
+                double z = p / (i + 1.0); // probability that the last index is i
+                seq_type r = hashes[p - 1][seq[i]];
+                bool s = signs[p - 1][seq[i]];
                 if (s) {
-                    T1[t] = shift_sum(T1[t], T1[t - 1], r, z);
-                    T2[t] = shift_sum(T2[t], T2[t - 1], r, z);
+                    Tp[p] = shift_sum(Tp[p], Tp[p - 1], r, z);
+                    Tm[p] = shift_sum(Tm[p], Tm[p - 1], r, z);
                 } else {
-                    T1[t] = shift_sum(T1[t], T2[t - 1], r, z);
-                    T2[t] = shift_sum(T2[t], T1[t - 1], r, z);
+                    Tp[p] = shift_sum(Tp[p], Tm[p - 1], r, z);
+                    Tm[p] = shift_sum(Tm[p], Tp[p - 1], r, z);
                 }
             }
         }
         std::vector<double> sketch(sketch_size, 0);
         for (uint32_t m = 0; m < sketch_size; m++) {
-            sketch[m] = T1[subsequence_len][m] - T2[subsequence_len][m];
+            sketch[m] = Tp[subsequence_len][m] - Tm[subsequence_len][m];
         }
         return sketch;
     }
 
+    /** Sets the hash and sign functions to predetermined values for testing */
     void set_hashes_for_testing(const Vec2D<seq_type> &h, const Vec2D<bool> &s) {
         hashes = h;
         signs = s;
     }
 
   protected:
+    /** Computes (1-z)*a + z*b_shift */
     std::vector<double> shift_sum(const std::vector<double> &a,
                                   const std::vector<double> &b,
                                   seq_type shift,
@@ -80,22 +99,6 @@ class Tensor {
             assert(result[i] <= 1 + 1e-5 && result[i] >= -1 - 1e-5);
         }
         return result;
-    }
-
-    virtual void rand_init() {
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<seq_type> rand_hash2(0, sketch_size - 1);
-        std::uniform_int_distribution<seq_type> rand_bool(0, 1);
-
-        hashes = new2D<seq_type>(subsequence_len, alphabet_size);
-        signs = new2D<bool>(subsequence_len, alphabet_size);
-        for (size_t h = 0; h < subsequence_len; h++) {
-            for (size_t c = 0; c < alphabet_size; c++) {
-                hashes[h][c] = rand_hash2(gen);
-                signs[h][c] = rand_bool(gen);
-            }
-        }
     }
 
     /** Size of the alphabet over which sequences to be sketched are defined, e.g. 4 for DNA */
