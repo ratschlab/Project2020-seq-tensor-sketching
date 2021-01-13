1f5c228f5b0c09088a89dd50ace700193c7c770d
Amir Joudaki <amir.judaki@gmail.com>
2021-01-13 18:30:05 +0100

    fixed time reporting for flatteners

--- util/timer.cpp
+++ util/timer.cpp
@@ -29,8 +29,8 @@ std::string timer_summary(uint32_t num_seqs, uint32_t num_pairs) {
             { "ordered_minhash", "OMH" },
             { "tensor_sketch", "TS" },
             { "tensor_slide_sketch", "TSS" },
-            {"tensor_slide_flat", "TSF"},
-            {"tensor_slide_binary", "TSB"},
+            {"Int32Flattener", "I32FLAT"},
+            {"DoubleFlattener", "FLAT"},
             {"seq2kmer", "S2K"}
     };
     std::string str = "long name,short name, time, time sketch, time dist\n";
@@ -44,26 +44,26 @@ std::string timer_summary(uint32_t num_seqs, uint32_t num_pairs) {
             }
         }
     }
-        for (auto const &[arg_name, arg] : acc) {
-            double count = (double)arg, count2;
-            // add kmer computation time to the sketch time of MH* methods
-            if (arg_name.find("hash") != std::string::npos && // contains *hash*
-                arg_name.find("dist") == std::string::npos) { // doesn't contain *dist*
-                count += acc["seq2kmer"];
-            }
-            if (arg_name == "edit_distance") {
-                count = count/1e6/num_pairs;
-                str = str + arg_name + "," + trans[arg_name] + "," + std::to_string(count) + ",0,0\n";
-            } else if (arg_name.find("dist") == std::string::npos && arg_name!="seq2kmer") {
-                // average sketching time + average distance computation time in milliseonds
-                count = count/1e6/num_seqs ;
-                count2 = acc[arg_name + "_dist"]/1e6/num_pairs;
-                str = str + arg_name + "," + trans[arg_name] +
-                        "," + std::to_string(count+count2) +
-                        "," + std::to_string(count) +
-                        "," + std::to_string(count2) + '\n';
-            }
+    for (auto const &[arg_name, arg] : acc) {
+        double count = (double)arg, count2;
+        // add kmer computation time to the sketch time of MH* methods
+        if (arg_name.find("hash") != std::string::npos && // contains *hash*
+            arg_name.find("dist") == std::string::npos) { // doesn't contain *dist*
+            count += acc["seq2kmer"];
         }
+        if (arg_name == "edit_distance") {
+            count = count/1e6/num_pairs;
+            str = str + arg_name + "," + trans[arg_name] + "," + std::to_string(count) + ",0,0\n";
+        } else if (arg_name.find("dist") == std::string::npos && arg_name!="seq2kmer") {
+            // average sketching time + average distance computation time in milliseonds
+            count = count/1e6/num_seqs ;
+            count2 = acc[arg_name + "_dist"]/1e6/num_pairs;
+            str = str + arg_name + "," + trans[arg_name] +
+                  "," + std::to_string(count+count2) +
+                  "," + std::to_string(count) +
+                  "," + std::to_string(count2) + '\n';
+        }
+    }
     return str;
 }
 
