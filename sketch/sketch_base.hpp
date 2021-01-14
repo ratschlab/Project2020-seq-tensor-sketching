d590a4459ca10505dd9679b8000e3434e0870e32
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2021-01-14 16:01:19 +0100

    cleanup

--- sketch/sketch_base.hpp
+++ sketch/sketch_base.hpp
@@ -8,8 +8,13 @@ namespace ts {
 template <typename sketch_type_, bool kmer_input_>
 class SketchBase {
   public:
+    // The type that the compute function returns.
     using sketch_type = sketch_type_;
+
+    // Whether the compute function takes a list of kmers.
     constexpr static bool kmer_input = kmer_input_;
+
+    // The name of the sketching algorithm.
     const std::string name;
 
     explicit SketchBase(std::string name) : name(std::move(name)) {}
