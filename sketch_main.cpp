f08cee739079ddc8ae97ba2b6a991454b7ba160a
ajoudaki <ajoudaki@inf.ethz.ch>
2021-01-25 15:13:42 +0000

    normalized Ten2, Ten3, and ED

--- sketch_main.cpp
+++ sketch_main.cpp
@@ -9,6 +9,7 @@
 #include <random>
 #include <sstream>
 #include <utility>
+#include "util/progress.hpp"
 
 DEFINE_string(o, "", "Output file");
 
@@ -31,9 +32,9 @@ DEFINE_validator(i, &ValidateInput);
 using namespace ts;
 
 namespace fs = std::filesystem;
-using Tensor23 = std::pair<std::vector<uint32_t>, std::vector<uint32_t>>;
+using Tensor23 = std::pair<std::vector<double>, std::vector<double>>;
 Tensor23 tensor23(std::vector<uint8_t> &seq) {
-    Tensor23 result({ std::vector<uint32_t>(16), std::vector<uint32_t>(64) });
+    Tensor23 result({ std::vector<double>(16, 0), std::vector<double>(64, 0) });
     std::transform(seq.begin(), seq.end(), seq.begin(), [](uint8_t a) { return (a > 3) ? 0 : a; });
     for (uint32_t i = 0; i < seq.size(); ++i) {
         const uint32_t idx1 = 16 * seq[i];
@@ -45,6 +46,12 @@ Tensor23 tensor23(std::vector<uint8_t> &seq) {
             result.first[(seq[i] << 2) + seq[j]]++;
         }
     }
+    double l1_t2 = std::accumulate(result.first.begin(), result.first.end(),decltype(result.first)::value_type(0));
+    for (auto &e : result.first)  e = e / l1_t2;
+
+    double l1_t3 = std::accumulate(result.second.begin(), result.second.end(),decltype(result.second)::value_type(0));
+    for (auto &e : result.second)   e = e / l1_t3;
+
     return result;
 }
 
@@ -69,31 +76,60 @@ int main(int argc, char *argv[]) {
         sequences[i] = { ff.sequences[0], fs::path(names[i]).filename() };
     }
     std::sort(sequences.begin(), sequences.end(),
-              [](auto &a, auto &b) { return a.second < b.second; });
+              [](auto &a, auto &b) { return a.first.size() < b.first.size(); }); // sort the strings to length
     std::cout << "Read " << sequences.size() << " fasta files" << std::endl;
 
     if (sequences.empty()) {
         std::exit(0);
     }
 
-    fs::remove_all(FLAGS_o + "2");
-    fs::remove_all(FLAGS_o + "3");
+    fs::remove_all(FLAGS_o);
     const size_t BLOCK_SIZE = FLAGS_b;
     std::vector<Tensor23> tensors(sequences.size());
+    std::ofstream f(FLAGS_o + "_ed");
+    f << "seq1,seq2,len1,len2,Ten2,Ten3,ED" << std::endl;
+
     for (uint32_t epoch = 0; epoch < (sequences.size() - 1) / BLOCK_SIZE + 1; ++epoch) {
         size_t max_ind = std::min((epoch + 1) * BLOCK_SIZE, sequences.size());
+        std::cout << "epoch " << epoch << std::endl;
+        std::cout << "computing sketches ... " << std::flush;
+        progress_bar::init(max_ind - epoch * BLOCK_SIZE );
 #pragma omp parallel for num_threads(FLAGS_t)
         for (uint32_t i = epoch * BLOCK_SIZE; i < max_ind; ++i) {
+            progress_bar::iter();
             tensors[i] = tensor23(sequences[i].first);
         }
-        std::ofstream f2(FLAGS_o + "2");
-        std::ofstream f3(FLAGS_o + "3");
-        for (uint32_t j = 0; j < max_ind; ++j) {
+
+        std::cout << "computing distances ... " << std::flush;
+        Vec3D<double> dists = new3D<double>(BLOCK_SIZE, BLOCK_SIZE, 7, 0);
+        progress_bar::init(max_ind * (max_ind-1) /2 );
+#pragma omp parallel for num_threads(FLAGS_t)
+        for (uint32_t j = epoch * BLOCK_SIZE; j < max_ind; ++j) {
             for (uint32_t k = j + 1; k < max_ind; ++k) {
-                f2 << sequences[j].second << "," << sequences[k].second << ", "
-                   << l2_dist(tensors[j].first, tensors[k].first) << std::endl;
-                f3 << sequences[j].second << "," << sequences[k].second << ", "
-                   << l2_dist(tensors[j].second, tensors[k].second) << std::endl;
+                progress_bar::iter();
+                auto &row = dists[j-epoch*BLOCK_SIZE][k-BLOCK_SIZE * epoch];
+                row[0] = j;
+                row[1] = k;
+                row[2] = sequences[j].first.size();
+                row[3] = sequences[k].first.size();
+                row[4] = l2_dist(tensors[j].first, tensors[k].first);
+                row[5] = l2_dist(tensors[j].second, tensors[k].second);
+                row[6] = edit_distance(sequences[j].first, sequences[k].first);
+            }
+        }
+        for (auto &arr : dists) {
+            for (auto & row : arr) {
+                if (row[0] >= row[1])
+                    continue;
+
+                for (size_t i = 0; i<7; i++ ) {
+                    if (i<4)
+                        f << (int)(row[i]) << ",";
+                    else if (i<=5)
+                        f << row[i] << ",";
+                    else if (i==6)
+                        f << row[i] << std::endl;
+                }
             }
         }
         std::cout << "Epoch " << epoch << " done." << std::endl;
